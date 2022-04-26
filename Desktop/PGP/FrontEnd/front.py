6c9961dcab03485c0c8d8071a4f2cd018b2e208e
Nilay <201901026@daiict.ac.in>
2022-04-26 15:37:16 +0530

    Updated file feature

--- Desktop/PGP/FrontEnd/front.py
+++ Desktop/PGP/FrontEnd/front.py
@@ -2,10 +2,22 @@ from turtle import onclick
 from main import *
 import pywebio.output as pwo
 import gnupg
+import pandas as pd
 import back
-
+import pywebio.input as pwi
 from pywebio.input import textarea
-def encrypt():
+def encrypt_file():
+    pwo.clear()
+    userfile=pwi.file_upload('Choose your file')
+    open(userfile['filename'],'wb').write(userfile['content'])
+    df=pd.read_csv(userfile['filename'], error_bad_lines=False)
+    main_data=df.to_string(index=False)
+    encrypted_data=back.encrypt_data(main_data)
+    pwo.clear()
+    pwo.put_markdown(""" # Encrypted Key""")
+    pwo.put_text(str(encrypted_data))
+    pwo.put_buttons(['Home Page'], onclick = [home_page])
+def encrypt_message():
     pwo.clear()
     main_data=textarea('Enter the text you want to encrypt:', type='text')
     encrypted_data=back.encrypt_data(main_data)
@@ -13,6 +25,14 @@ def encrypt():
     pwo.put_markdown(""" # Encrypted Key""")
     pwo.put_text(str(encrypted_data))
     pwo.put_buttons(['Home Page'], onclick = [home_page])
+
+def encrypt():
+    pwo.clear()
+    pwo.put_markdown(""" # Enter a secret file""")
+    pwo.put_buttons(['Add file'], onclick = [encrypt_file])
+    pwo.put_markdown(""" # Enter a secret message""")
+    pwo.put_buttons(['Add message'], onclick = [encrypt_message])
+    
 def decrypt():
     pwo.clear()
     encrypt_data=textarea('Enter the text you want to decrypt:', type='text')
