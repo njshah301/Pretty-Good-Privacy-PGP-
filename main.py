8cf08df070a7fc7c1729caf143abfae87af9ee76
Ayush Prajapati <58745662+ayushprajapati@users.noreply.github.com>
2022-05-12 22:36:46 +0530

    Update main.py
    Background image

--- main.py
+++ main.py
@@ -5,21 +5,8 @@ def home_page():
     import pywebio.output as pwo
     from pywebio.input import input
     pwo.clear() 
-    pwo.put_html('''<!DOCTYPE html>
-<html>
-<head>
-<style>
-body {
-  background-image: url('https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80');
-}
-
-}
-</style>
-</head>
-<body>
-</body>
-</html>
-''' )
+    ig=open("1.jpg",'rb').read()
+    pwo.put_image(ig)
     pwo.set_scope('List')
     pwo.put_markdown(""" # Pretty Good Privacy (PGP)""")
     pwo.put_buttons(['Encryption'], onclick = [front.encrypt])
